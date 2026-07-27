0f4ed9ad2bb2fff397ecc08bf42fbaaa5d70283b
Rohit kadam <rohitkadam1230@gmail.com>
2026-07-27 22:19:30 +0530

    Time: 3 ms (17.59%), Space: 44 MB (13.34%) - LeetHub

--- 1652-defuse-the-bomb/1652-defuse-the-bomb.java
+++ 1652-defuse-the-bomb/1652-defuse-the-bomb.java
@@ -1,29 +1,31 @@
 class Solution {
     public int[] decrypt(int[] code, int k) {
-        int n=code.length;
-        int[] ans=new int[n];
+        int n = code.length;
+        int[] ans = new int[n];
 
-        if(k==0){
-            return ans;
-        }
-
-        for(int i=0;i<n;i++){
-            int sum=0;
-            if(k>0){
-                for(int j=1;j<=k;j++){
-                    int idx=(i+j)%n;
-                    sum+=code[idx];
-                } 
-            }else{
-                for(int j=1;j<=-k;j++){
-                    int idx=(i-j+n)%n;
-                    sum+=code[idx];
-                }
+        if(k == 0){
+            for(int i=0;i<n;i++){
+                ans[i] = 0;
             }
-            ans[i]=sum;
         }
-
+        else if(k > 0){
+            for (int i = 0; i < n; i++) {
+                int sum = 0;
+                for (int j = 1; j <= k; j++) {
+                    sum += code[(i + j) % n]; // Wraps forward automatically
+                }
+                ans[i] = sum;
+            }
+        }else{
+            int absK = Math.abs(k); // or -k
+            for (int i = 0; i < n; i++) {
+                int sum = 0;
+                for (int j = 1; j <= absK; j++) {
+                    sum += code[(i - j + n) % n]; // Wraps backward automatically
+                }
+                ans[i] = sum;
+            }
+        }
         return ans;
-
     }
 }
