cf9d9cf2c487d5149fc0a125dc28dfd21adc18c9
Rohit kadam <rohitkadam1230@gmail.com>
2026-08-04 22:10:13 +0530

    Time: 11 ms (39.32%), Space: 46.4 MB (67.95%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        char mag[]=magazine.toCharArray();
+
+        for(int i=0;i<ransomNote.length();i++){
+            boolean found=false;
+            for(int j=0;j<magazine.length();j++){
+                if(ransomNote.charAt(i)==mag[j]){
+                    mag[j]='#';
+                    found=true;
+                    break;
+                }
+            }
+
+            if(!found){
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
