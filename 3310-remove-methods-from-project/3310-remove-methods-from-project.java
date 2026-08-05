55430c39b68662e9663b6cff47908454143d0150
Rohit kadam <rohitkadam1230@gmail.com>
2026-08-05 10:01:33 +0530

    Time: 80 ms (47.12%), Space: 290.1 MB (42.31%) - LeetHub

---
+++ 3310-remove-methods-from-project/3310-remove-methods-from-project.java
@@ -0,0 +1,54 @@
+class Solution {
+    public void dfs(int u,List<List<Integer>> grph,boolean vis[]){
+        vis[u]=true;
+
+        for(int v:grph.get(u)){
+            if(vis[v]){
+                continue;
+            }
+            dfs(v,grph,vis);
+        }
+    }
+    public List<Integer> remainingMethods(int n, int k, int[][] invocations) {
+        List<List<Integer>> grph=new ArrayList<>();
+        
+        for(int i=0;i<n;i++){
+            grph.add(new ArrayList<>());
+        }
+
+        for(int[] arr:invocations){
+            int u=arr[0];
+            int v=arr[1];
+            grph.get(u).add(v);
+        }
+
+        boolean vis[]=new boolean[n];
+        dfs(k,grph,vis);
+
+        for(int arr[]:invocations){
+            int u=arr[0];
+            int v=arr[1];
+
+            if(vis[u])continue;
+
+            if(vis[v]){
+                List<Integer> ans=new ArrayList<>();
+                for(int i=0;i<n;i++){
+                    ans.add(i);
+                }
+                return ans;
+            }
+        }
+
+        List<Integer> ans=new ArrayList<>();
+        for(int i=0;i<n;i++){
+            if(!vis[i]){
+                ans.add(i);
+            }
+        }
+
+        return ans;
+
+
+    }
+}
