ef4ae4c2866d11a783cfa4aab0ff8d9c2b8c9b4f
Rohit kadam <rohitkadam1230@gmail.com>
2026-08-10 10:41:26 +0530

    Time: 8 ms (24.45%), Space: 45.3 MB (87.44%) - LeetHub

--- 0657-robot-return-to-origin/0657-robot-return-to-origin.java
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.java
@@ -1,25 +1,22 @@
 class Solution {
     public boolean judgeCircle(String moves) {
-        int[] ans=new int[]{0,0};
+        int x=0,y=0;
 
         for(int i=0;i<moves.length();i++){
             if(moves.charAt(i)=='U'){
-                ans[0]+=1;
+                x+=1;
             }else if(moves.charAt(i)=='D'){
-                ans[0]-=1;
+                x-=1;
             }else if(moves.charAt(i)=='R'){
-                ans[1]+=1;
+                y+=1;
             }else if(moves.charAt(i)=='L'){
-                ans[1]-=1;
+                y-=1;
             }
         }
 
-        boolean finalAns=false;
-        if(ans[0]==0 && ans[1]==0){
-            finalAns=true;
-        }
+        
 
-        return finalAns;
+        return x==0 && y==0?true:false;
         
     }
 }
