a28a07d2aa03413ea374b40f54bffa5941d734c4
Rohit kadam <rohitkadam1230@gmail.com>
2026-08-23 19:43:19 +0530

    Time: 6 ms (97.08%), Space: 46.9 MB (64.23%) - LeetHub

--- 1927-sum-game/1927-sum-game.java
+++ 1927-sum-game/1927-sum-game.java
@@ -1,38 +1,42 @@
 class Solution {
     public boolean sumGame(String num) {
-        int n=num.length();
-        int half=n/2;
-        int q1=0;
-        int q2=0;
+        int n = num.length();
+        int half = n / 2;
 
-        int leftSum=0;
-        int rightSum=0;
+        int leftSum = 0;
+        int rightSum = 0;
+        int leftQ = 0;
+        int rightQ = 0;
 
-        for(int i=0;i<half;i++){
-            if(num.charAt(i)=='?'){
-                q1++;
-            }else{
-                leftSum+=num.charAt(i)-'0';
+        for (int i = 0; i < half; i++) {
+            char c = num.charAt(i);
+
+            if (c == '?') {
+                leftQ++;
+            } else {
+                leftSum += c - '0';
             }
         }
 
-        for(int i=half;i<n;i++){
-            if(num.charAt(i)=='?'){
-                q2++;
-            }else{
-                rightSum+=num.charAt(i)-'0';
+        for (int i = half; i < n; i++) {
+            char c = num.charAt(i);
+
+            if (c == '?') {
+                rightQ++;
+            } else {
+                rightSum += c - '0';
             }
         }
 
-        int q=q1+q2;
-        if(q%2!=0){
+        int qDiff = leftQ - rightQ;
+        int sumDiff = leftSum - rightSum;
+
+      
+        if (qDiff % 2 != 0) {
             return true;
         }
 
-        int diff=leftSum-rightSum;
-        int target=q1-q2;
-
-        return diff+9*(target/2)!=0;
-
+      
+        return sumDiff + 9 * (qDiff / 2) != 0;
     }
 }
