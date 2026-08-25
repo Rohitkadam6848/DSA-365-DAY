f386ac9f6e106e7bbf4ed65b9a22ca984765c0e8
Rohit kadam <rohitkadam1230@gmail.com>
2026-08-25 16:51:39 +0530

    Time: 0 ms (100%), Space: 44.8 MB (83.31%) - LeetHub

--- 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.java
+++ 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.java
@@ -1,18 +1,17 @@
 class Solution {
     public int missingMultiple(int[] nums, int k) {
-       HashSet<Integer> set=new HashSet<>();
-
-       for(int num:nums){
-            set.add(num);
-        } 
-
-        int ans=k;
-
-        while(set.contains(ans)){
-            ans+=k;
+        for(int multiple=k;;multiple+=k){
+            boolean found=false;
+            for(int i=0;i<nums.length;i++){
+                if(nums[i]==multiple){
+                    found=true;
+                    break;
+                }
+            }
+            if(!found){
+            return multiple;
+            }
         }
-
-        return ans;
-
+        
     }
 }
