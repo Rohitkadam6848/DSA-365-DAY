73b62e46bac46069cff0b11bf8f16ae2ba5d7601
Rohit kadam <rohitkadam1230@gmail.com>
2026-09-09 16:44:23 +0530

    Time: 0 ms (100%), Space: 42.1 MB (79.43%) - LeetHub

---
+++ 2139-minimum-moves-to-reach-target-score/2139-minimum-moves-to-reach-target-score.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int minMoves(int target, int maxDoubles) {
+        int moves=0;
+        int num=target;
+
+        while(num>1){
+            if(maxDoubles==0){
+                moves+=num-1;
+                break;
+            }
+            if(num%2==0 && maxDoubles>0){
+                maxDoubles--;
+                moves++;
+                num/=2;
+                
+            }else{
+                num--;
+                moves++;
+            }
+
+           
+        }
+
+        return moves;
+    }
+}
